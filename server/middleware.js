1c4d477aff2fc3fab35125cd105079dc0332561c
nPelvin <pelvinpt@gmail.com>
2022-01-11 22:53:51 +0000

    applied csp to middleware.js

--- server/middleware.js
+++ server/middleware.js
@@ -9,6 +9,7 @@ export const configuredHelmet = () =>
 				objectSrc: ["'none'"],
 				scriptSrc: ["'self'", "unpkg.com", "polyfill.io"],
 				styleSrc: ["'self'", "https: 'unsafe-inline'"],
+				imageSrc: ["lh3.googleusercontent.com"]
 				upgradeInsecureRequests: [],
 			},
 		},
